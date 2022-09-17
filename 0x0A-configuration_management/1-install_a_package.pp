a59889fd48c7702e16b0418ea06c776d1329c1e5
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-09-16 18:53:55 -0700

    add some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+# install flask -v 2.1.0
+
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
